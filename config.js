281ee080bcc50789ea411e4dc16f3f523663261d
Ravshan <rbazarbaev@gmail.com>
2017-04-11 17:11:39 +0600

    Bot id changed

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var config = {
-    // token: '210039703:AAFjqrxT3EyLpRJGxfPP4_ob3ZSjp1vC4k0', //token for telegram bot MD @mdtime_bot
-    token: '342706056:AAGUu9tR287bJoD2B83lYu3gcPa_2o4RIwg', // test @mdChecklist2_bot
+    token: '210039703:AAFjqrxT3EyLpRJGxfPP4_ob3ZSjp1vC4k0', //token for telegram bot MD @mdtime_bot
+    // token: '342706056:AAGUu9tR287bJoD2B83lYu3gcPa_2o4RIwg', // test @mdChecklist2_bot
     botName: '@mdtime_bot',
     // mongoUrl: 'mongodb://127.0.0.1:27017/checklistTest',
     mongoUrl: 'mongodb://127.0.0.1:27017/mdchecklist', //
