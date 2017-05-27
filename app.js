575909fee289948b3d8af37d6268646cecdb7bfc
Ravshan <rbazarbaev@gmail.com>
2017-05-27 18:22:03 +0600

    Cors enabled only to pmd

--- app.js
+++ app.js
@@ -15,12 +15,16 @@ var mongoose = require('mongoose');
 
 var config = require('./config');
 var app = express();
-// app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+var corsOpt = {
+  origin: "http://people.mirusdesk.kz",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOpt));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 //if mongo with auth
 // mongoose.connect(config.mongoUrl, config.opt);
 //mongoose connection
